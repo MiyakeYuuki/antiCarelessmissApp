73bb17d0fc6eed730f81ece80bac0e7d363d95c0
MiyakeYuuki <research.myky@gmail.com>
2024-03-17 10:58:09 +0900

    :sparkles:first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Login from "./pages/Login/Login";
+// import { ChatProvider } from "./pages/Chat/ChatProvider";
+
+const App = () => {
+  return (
+    // <ChatProvider>
+    <Login />
+    // </ChatProvider>
+  );
+};
+
+export default App;
